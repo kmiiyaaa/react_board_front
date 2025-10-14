1df89c4de13f2877a56519920b477fca27676750
kmiiyaaa <ckm1533@naver.com>
2025-10-14 15:05:04 +0900

    특정게시글 세부 내용보기 추가

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -6,15 +6,22 @@ import "./BoardWrite.css";
 
 function Board({ user }) {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   //게시판 모든글 요청
   const laodPosts = async () => {
     try {
+      setLoading(true);
       const res = await api.get("/api/board"); //모든 게시글 가져오기 요청
       setPosts(res.data); // posts -> 전체 게시글 -> 게시글의 배열
     } catch (err) {
       console.error(err);
+      setError("게시글을 불러오는 데 실패하였습니다.");
+      setPosts([]); //게시글들의 배열 다시 초기화
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -41,6 +48,9 @@ function Board({ user }) {
   return (
     <div className="container">
       <h1>게시판</h1>
+
+      {loading && <p>게시판 글 리스트 로딩 중...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <table className="board-table">
         <thead>
           <tr>
--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -1,27 +1,60 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../api/axiosConfig";
 
 function BoardDetail({ user }) {
   const navigate = useNavigate();
 
   const [post, setPost] = useState(null); //해당 글 id로 요청한 글 객체 -> 객체라서 "" 안되고 null 넣어줘야한다
+  const [loading, setLodaing] = useState(true);
+  const [error, setError] = useState(null);
+  const { id } = useParams(); // board/:id  id 파라미터 받아오기
 
   const laodPost = async () => {
     // 특정글 id로 글1개 요청하기
+    try {
+      setLodaing(true);
+      const res = await api.get(`/api/board/${id}`);
+      setPost(res.data); // 특정글 id 객체를 state에 등록
+      console.log(res.data.title);
+    } catch (err) {
+      console.error(err);
+      setError("해당 게시글은 존재하지 않습니다.");
+      //alert("해당 게시글은 존재하지 않습니다.");
+    } finally {
+      setLodaing(false);
+    }
   };
+
+  useEffect(() => {
+    laodPost();
+  }, [id]);
+
+  if (loading) return <p>게시글 로딩중...</p>;
+  if (error) return <p style={{ color: "red" }}>{error}</p>;
+  if (!post)
+    return <p style={{ color: "blue" }}>해당 게시글이 존재하지 않습니다.</p>;
+
+  //로그인 상태이면서 로그인한 유저와 글을 쓴 유저가 동일할 때
+  const isAuthor = user && user === post.author.username;
 
   return (
     <div className="detail-container">
-      <h2>[제목]오늘 첫글</h2>
-      <p className="author">작성자 : tiger</p>
-      <div className="content">[글내용] 글내용 입니다</div>
+      <h2>{post.title}</h2>
+      <p className="author">작성자 : {post.author.username}</p>
+      <div className="content">{post.content}</div>
 
       <div className="button-group">
         <button onClick={() => navigate("/board")}>글목록</button>
 
         {/*로그인한 유저가 본인이 쓴 글만 수정,삭제 가능 */}
-        <button>수정</button>
-        <button>삭제</button>
+
+        {isAuthor && (
+          <>
+            <button>수정</button>
+            <button>삭제</button>
+          </>
+        )}
       </div>
     </div>
   );
