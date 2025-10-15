21a0a3ea39db2d7dd24d45baab1f1cd13456aac9
kmiiyaaa <ckm1533@naver.com>
2025-10-15 11:50:43 +0900

    유효성 체크 수정

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -13,7 +13,6 @@ function BoardDetail({ user }) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-<<<<<<< HEAD
   const { id } = useParams(); // board/:id  id 파라미터 받아오기
 
   //클 //클릭한 글의 id로 글 1개 가져오기
@@ -33,62 +32,13 @@ function BoardDetail({ user }) {
       // alert("해당 게시글은 존재하지 않습니다.");
     } finally {
       setLoading(false);
-=======
-  //클 //클릭한 글의 id로 글 1개 가져오기
-    const loadPost = async () => { //특정 글 id로 글 1개 요청하기
-        try{
-            setLoading(true);
-            const res = await api.get(`/api/board/${id}`);            
-            setPost(res.data); //특정 글 id 객체를 state에 등록
-            setTitle(res.data.title); 
-            //원본 글의 제목을 수정화면에 표시하는 변수인 title 변수에 저장
-            setContent(res.data.content);
-            //원본 글의 내용을 수정화면에 표시하는 변수인 content 변수에 저장
-            
-        } catch(err) {
-            console.error(err);
-            setError("해당 게시글은 존재하지 않습니다.");
-            // alert("해당 게시글은 존재하지 않습니다.");
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        loadPost();
-    },[id]);
-
-   //글삭제
-    const handleDelete = async () => {
-        if(!window.confirm("정말 삭제하시겠습니까?")) { //확인->true, 취소->false
-            return;
-        }
-        try {
-            await api.delete(`/api/board/${id}`);
-            alert("게시글 삭제 성공!");
-            navigate("/board");
-        } catch (err) {
-            console.error(err);
-            if(err.response.status === 403){
-                alert("삭제 권한이 없습니다.");
-            } else {
-                alert("삭제 실패!");
-            }
-        }
->>>>>>> 3d9bcdadbe63fa73c36a5d0f043b78aa3c52639a
     }
+  };
 
-   const handleUpdate = async () => {
-        if(!window.confirm("정말 수정하시겠습니까?")) { //확인->true, 취소->false
-            return;
-        }
-        try {
-            const res = await api.put(`/api/board/${id}`,{ title, content });
-            alert("게시글이 수정되었습니다.");
-            setPost(res.data); //새로 수정된 글로 post 변수 값 변경
-            setEditing(false); //상세보기 화면으로 전환
+  useEffect(() => {
+    loadPost();
+  }, [id]);
 
-<<<<<<< HEAD
   //글삭제
   const handleDelete = async () => {
     if (!window.confirm("정말 삭제하시겠습니까?")) {
@@ -190,60 +140,6 @@ function BoardDetail({ user }) {
       )}
     </div>
   );
-=======
-        } catch(err) {
-            console.error(err);
-            if(err.response.status === 403){
-                alert("수정 권한이 없습니다.");
-            } else {
-                alert("수정 실패!");
-            }
-        }
-    };
-
-    if(loading) return <p>게시글 로딩 중....</p>;
-    if(error) return <p style={{color:"red"}}>{error}</p>
-    if(!post) return <p sytle={{color:"blue"}}>해당 게시글이 존재하지 않습니다.</p>
-
-    //로그인 상태이면서 로그인한 유저와 글을 쓴 유저가 같은때->참
-    const isAuthor = user && user === post.author.username;
-
-    return (
-        <div className="detail-container">
-            {editing ? (
-                <div className="edit-form">
-                    <h2>글 수정하기</h2>
-                    <input type="text" value={title}
-                    onChange={(e) => setTitle(e.target.value)} />
-                    <textarea value={content}
-                    onChange={(e) => setContent(e.target.value)} />
-                    <div className="button-group">
-                        <button className="edit-button" onClick={handleUpdate}>저장</button>
-                        <button className="delete-button" onClick={() => setEditing(false)}>취소</button>
-                    </div>    
-                </div>
-            ):(
-                <>
-                <h2>{post.title}</h2>
-                <p className="author">작성자 : {post.author.username}</p>
-                <div className="content">{post.content}</div>
-
-                <div className="button-group">
-                    <button className="list-button" onClick={()=>navigate("/board")}>글목록</button>
-                    
-                    {/* 로그인한 유저 본인이 쓴글만 삭제 수정 가능 */}
-                    {isAuthor && (
-                    <>    
-                        <button className="edit-button" onClick={() => setEditing(true)}>수정</button>
-                        <button className="delete-button" onClick={handleDelete}>삭제</button>
-                    </>
-                    )}
-                </div>
-                </>
-            )}
-        </div>
-    );
->>>>>>> 3d9bcdadbe63fa73c36a5d0f043b78aa3c52639a
 }
 
 export default BoardDetail;
