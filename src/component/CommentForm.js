1430f23ed6b77f3dd9fb9460b5dcb9b6f3b6c6e8
kmiiyaaa <ckm1533@naver.com>
2025-10-17 09:41:45 +0900

    댓글쓰기 - commentForm 컴포넌트 분리

--- src/component/CommentForm.js
+++ src/component/CommentForm.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import api from "../api/axiosConfig";
 
-const CommentForm({user,content}) {
+const CommentForm({user,loadComments, boardId}) {
     const [newComment, setNewComment] = useState(""); // 새로운 댓글 저장 변수
+    const [commentErrors, setCommentErrors] = useState({});  // {} -> 객체 초기값
 
     //댓글 쓰기 함수 -> 원 게시글 id를 파라미터로 제출
-    const handleCommentSubmit = async (e) => {
-    // 백엔드에 댓글 저장 요청
+    const handleCommentSubmit = async (e) => {   // 백엔드에 댓글 저장 요청
     e.preventDefault(); //초기화
     setCommentErrors({});
+    
     if (!user) {
       alert("로그인 후 댓글 작성해주세요");
     }
@@ -18,10 +20,10 @@ const CommentForm({user,content}) {
 
     try {
       alert("댓글을 입력하시겠습니까?"); // 원래는 confirm 사용해야함
-      await api.post(`/api/comments/${id}`, { content: newComment }); //여기서 id는 원게시글의 id
+      await api.post(`/api/comments/${boardId}`, { content: newComment }); //여기서 boardId는 게시글의 id
       setNewComment("");
 
-      //댓글 리스트 불러오기 호출
+    //댓글 리스트 불러오기 호출
       loadComments(); //새 댓글 기존 댓글 리스트에 반영
     } catch (err) {
       if (err.response && err.response.status === 400) {
@@ -33,9 +35,9 @@ const CommentForm({user,content}) {
     }
   };
 
-    return(
-      
+    return( 
         <>
+          <h3>댓글 쓰기</h3>
           <form onSubmit={handleCommentSubmit} className="comment-form">
               <textarea
                 placeholder="댓글을 입력하세요"
