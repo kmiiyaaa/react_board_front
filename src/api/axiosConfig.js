6b4e12eb31bb4b7ad268404fa666f67afa44488d
kmiiyaaa <ckm1533@naver.com>
2025-10-20 09:26:31 +0900

    url 경로 수정

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.30.1.47:7777", //스프링부트 백엔드 기본 url
+  baseURL: "http://172.30.1.47:7777/api", //스프링부트 백엔드 기본 url
   withCredentials: true, //세션 쿠키 전달
 });
 
